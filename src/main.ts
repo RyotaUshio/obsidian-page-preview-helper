c9e2c1734766cbc33ee01c58b39de036a8a909df
RyotaUshio <ushio@ms.k.u-tokyo.ac.jp>
2024-02-18 00:28:15 +0900

    release: 0.1.0

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,70 @@
-import { Plugin } from 'obsidian';
-import { MyPluginSettings, DEFAULT_SETTINGS, SampleSettingTab } from 'settings';
+import { HoverParent, Plugin } from 'obsidian';
+import { around } from 'monkey-around';
 
+
+interface HoverLinkContext {
+	event: MouseEvent;
+	source: string;
+	hoverParent: HoverParent;
+	targetEl?: HTMLElement;
+	linktext: string;
+	sourcePath?: string;
+	state?: any;
+}
+
+interface PagePreviewPlugin {
+	instance: {
+		onHoverLink(ctx: HoverLinkContext): void;
+	}
+	enabled: boolean;
+	enable(): void;
+	disable(): void;
+}
+
+declare module 'obsidian' {
+	interface App {
+		internalPlugins: {
+			plugins: {
+				'page-preview': PagePreviewPlugin;
+			}
+		};
+	}
+}
 
 export default class MyPlugin extends Plugin {
 	settings: MyPluginSettings;
 
 	async onload() {
-		await this.loadSettings();
-		await this.saveSettings();
-		this.addSettingTab(new SampleSettingTab(this));
+		this.app.workspace.onLayoutReady(() => {
+			const pagePreview = this.app.internalPlugins.plugins['page-preview'];
+			this.patchPagePreview(pagePreview);
+			this.refleshPagePreview(pagePreview);	
+		});
 	}
 
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+	patchPagePreview(pagePreview: PagePreviewPlugin) {
+		this.register(around(pagePreview.instance.constructor.prototype, {
+			onHoverLink(old) {
+				return function (ctx: HoverLinkContext) {
+					if (ctx.source === 'preview' && ctx.targetEl && ctx.targetEl.closest('.is-live-preview')) {
+						ctx.source = 'editor';
+					}
+					return old.call(this, ctx);
+				}
+			}
+		}));
 	}
 
-	async saveSettings() {
-		await this.saveData(this.settings);
+	/** Re-register the `workspace.on("hover-link", ...)` event handler so that the patch takes effect. */
+	refleshPagePreview(pagePreview: PagePreviewPlugin) {
+		pagePreview.disable();
+		pagePreview.enable();
+
+		this.register(() => {
+			if (pagePreview.enabled) {
+				pagePreview.disable();
+				pagePreview.enable();
+			}
+		});
 	}
 }
